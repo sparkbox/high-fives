e7b5821f007cd4d8d907e2f1941bef0c9322c0cc
ethanmuller <ethan@heysparkbox.com>
2017-10-06 11:44:18 -0400

    wip

--- helpers/index.js
+++ helpers/index.js
@@ -1,27 +1,69 @@
+const Handlebars = require('handlebars');
+const templates = require('../message-templates');
+
 // I guess all custom helpers can live in this file until it gets too big
 // ¯\_(ツ)_/¯
 
-function foo() {
-  return "Look! A custom helper";
+function eachWithUniqueTemplateIndex(collection, options) {
+  // This loops through a given collection, and adds a unique, random index
+  // referencing a template. This is used to make sure no templates are repeated
+  // within a collection.
+
+  if (collection.length > templates.length) {
+    throw new Error(`There aren't enough templates to give unique template indices for each item. There are ${collection.length} items in the collection, but only ${templates.lenth} templates.`);
+  }
+
+  const pickedNumbers = [];
+
+  const ret = collection.map((item) => {
+    const modifiedItem = Handlebars.createFrame(item)
+
+    let pickedNumber =  Math.floor(Math.random()*templates.length);
+
+    // Sorry for using a while loop
+    while(pickedNumbers.indexOf(pickedNumber) > -1) {
+      pickedNumber =  Math.floor(Math.random()*templates.length);
+    }
+
+    modifiedItem.templateIndex = pickedNumber;
+
+    // Store the picked number, so we don't pick it again
+    pickedNumbers.push(pickedNumber);
+
+    return options.fn(modifiedItem);
+  }).join('');
+
+  return ret;
 }
 
-function bar(options) {
-  // Here's an example of how you do a block helper, for when you
-  // inevitably forget how to do a block helper
+function getRandomTemplate(options) {
+  Handlebars.registerHelper({
+    firstName: () => this.name.split(' ')[0],
+    heShe: () => this.heShePronoun,
+    hisHer: () => this.hisHerPronoun,
+    articleLink: () => {
+      return new Handlebars.SafeString(
+        `<a href="${this.article.url}">${this.article.title}</a>`
+      );
+    }
+  });
 
-  return `START BLOCK
-  ${options.fn(this)}
-  END BLOCK`;
+  const rawRandomTemplate = templates[Math.floor(Math.random()*templates.length)];
+  const randomTemplate = Handlebars.compile(rawRandomTemplate);
+
+  return randomTemplate(this);
 }
 
-function addToContext(options) {
-  const newContext = Object.assign({}, this, { thing: 'IT WORKS' });
+function getTemplate(index, options) {
+}
 
-  return options.fn(newContext);
+function prettyNum(number, options) {
+  // https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 module.exports = {
-  foo: foo,
-  bar: bar,
-  addToContext: addToContext,
+  getRandomTemplate,
+  eachWithUniqueTemplateIndex,
+  prettyNum
 }
---
+++ message-templates/index.js
@@ -0,0 +1,13 @@
+module.exports = [
+  "{{firstName}} won hearts and changed minds when {{heShe}} wrote {{articleLink}}.",
+  "{{firstName}} permanently changed the Internet when {{heShe}} wrote {{articleLink}}.",
+  "{{firstName}} is arguably one of the best people out there. Want proof? Read {{articleLink}}.",
+  "{{firstName}} deserves a high five for writing {{articleLink}}.",
+  "{{firstName}} is the famed author of {{articleLink}}.",
+  "{{firstName}} blew up the blogosphere with {{articleLink}}.",
+  "{{firstName}} wrote {{articleLink}}. Some say it's the next <a href=\"https://alistapart.com/article/dao\">A Dao of Web Design</a>.",
+  "{{firstName}} is all anybody's ever talking about. I'm almost tired of hearing how great {{articleLink}} is. Just kidding. I'll never get tired of it.",
+  "{{firstName}} has a gift. {{articleLink}} is proof.",
+  "{{firstName}} is your Next Great Thought Leader™. {{articleLink}} is {{hisHer}} magnum opus.",
+  "{{firstName}} wrote the very popular {{articleLink}}. What a treasure!",
+];
