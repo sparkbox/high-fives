47e21323ea8a06054e5b349b26f58d5267517e05
ethanmuller <ethan@heysparkbox.com>
2018-08-24 11:28:21 -0400

    wip: add google analytics

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const globby = require('globby');
+const path = require('path');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const jsPath = path.resolve('./src/js');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.join(__dirname, './dist/js'),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          { loader: 'babel-loader', options: { presets: ['env'] } }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new MinifyPlugin()
+  ]
+};
