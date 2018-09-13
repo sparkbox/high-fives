49f2deafbf132866f1ab266028bfb94c1827e9e5
ethanmuller <ethan@heysparkbox.com>
2018-09-12 22:21:04 -0400

    feat: Slackbot & spreadsheet stuff

--- routes/index.js
+++ routes/index.js
@@ -13,10 +13,56 @@ const getData = () => {
   })
 }
 
+const mixData = (postAnalyticsData, messageData) => {
+  // This mixes our set of data into
+  // a nice little salad of happiness
+  //
+  // It merges the two arrays by alternating through
+  // the items, merging them into one list.
+  //
+  // Right now it only supports mixing two lists.
+
+  const arrA = [...postAnalyticsData]
+  const arrB = [...messageData]
+  const arrArr = [arrA, arrB]
+  const limit = arrArr.reduce((a, b) => a + b.length, 0)
+  const mixedData = []
+
+  for (let i = 0; i < limit; i++) {
+    const nextItem = arrArr[i % 2].shift()
+
+    if (nextItem) {
+      mixedData.push(nextItem)
+    } else {
+      const nextNextItem = arrArr[(i+1) % 2].shift()
+      if (nextNextItem) {
+        mixedData.push(nextNextItem)
+      }
+    }
+  }
+
+  // console.log(mixedData)
+  // return postAnalyticsData
+  return mixedData
+}
+
 router.get('/', async (req, res, next) => {
   const { data, fetchedAt } = await getData()
-  const shuffledData = shuffle(data)
-  const firstFiveShuffledItems = shuffledData.slice(0, 5)
+  const { postAnalyticsData, messageData } = data
+
+  const typedPostAnalyticsData = postAnalyticsData.map(i => { return {
+    ...i,
+    type: "analytics"
+  }})
+  const typedMessageData = messageData.map(i => { return {
+    ...i,
+    type: "message"
+  }})
+
+  const mixedData = mixData(typedPostAnalyticsData, typedMessageData)
+  // console.log(mixedData)
+  // const shuffledData = shuffle(data.postAnalyticsData)
+  const firstFiveShuffledItems = mixedData.slice(0, 5)
 
   const title = `High Fives`
 
---
+++ slackbot.js
@@ -0,0 +1,101 @@
+// following this mostly: https://slackapi.github.io/node-slack-sdk/rtm_api
+require('dotenv').config()
+
+const colors = require('colors')
+const { RTMClient, WebClient } = require('@slack/client')
+const submitMessage = require('./lib/submit-message')
+
+const token = process.env.SLACK_TOKEN
+
+const validApprovers = [
+  'UCLPFL5NC',
+]
+
+const channelsToWatch = [
+  'CCP5BTQAW',
+]
+
+const isValidReaction = (reaction) => {
+  return !!reaction.match(/earth_/)
+}
+
+const isValidMessage = (message) => {
+  const userMentionRegex = /<@(U[A-Z0-9]+)>/
+  return !!message.text.match(userMentionRegex)
+}
+
+const isValidApprover = a => validApprovers.includes(a)
+const isValidAuthor = s => !!s
+const inChannelsToWatch = c => channelsToWatch.includes(c)
+
+const rtm = new RTMClient(token)
+rtm.start()
+
+const web = new WebClient(token)
+
+const getUserList = () => {
+  return web.users.list()
+}
+
+const getUserFromList = (list, userId) => {
+  return list.members.find(m => m.id === userId)
+}
+
+const getMessage = async (channel, ts, userId) => {
+  // The best way I could find to grab a specific message is by
+  // timestamp. You can ask slack for the history of a channel,
+  // and scope it down to an exact timestamp. This will, hopefully,
+  // give you an array containing a single message with the timestamp
+  // you asked for. There's probably a better way to do this, but...
+  // ¯\_(ツ)_/¯
+  const { messages } = await web.channels.history({
+    channel,
+    inclusive: true,
+    oldest: ts,
+    latest: ts,
+  })
+
+  return messages.find(m => m.ts === ts && m.user === userId)
+}
+
+rtm.on('authenticated', async (e) => {
+  console.log(`Slackbot is authenticated. ${"Let's boogie.".bold.magenta}`)
+})
+
+rtm.on('reaction_added', async (e) => {
+  const { channel } = e.item
+  const approverId = e.user
+  const authorId = e.item_user
+  const relevantEvent = (isValidApprover(approverId) &&
+                         isValidAuthor(authorId) &&
+                         inChannelsToWatch(channel) &&
+                         isValidReaction(e.reaction))
+
+  if (relevantEvent) {
+    const { ts } = e.item
+    const message = await getMessage(channel, ts, authorId)
+
+    if (!isValidMessage(message)) {
+      console.log("It looks like somebody tried to approve this message, but nobody is mentioned?", message)
+    }
+
+    console.log("Message approved:", message)
+
+    const userList = await getUserList()
+    const author = getUserFromList(userList, authorId)
+    const approver = getUserFromList(userList, approverId)
+    const data = {
+      authorName: author.real_name,
+      authorId,
+      approverName: approver.real_name,
+      approverId,
+      ts,
+      messageText: message.text,
+      clientMsgId: message.client_msg_id
+    }
+
+    const opts = { userList }
+
+    submitMessage(data, opts)
+  }
+})
