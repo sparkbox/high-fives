1e711ff9a948baff616dcb88d6eb8bc17a654883
ethanmuller <ethan@heysparkbox.com>
2018-09-13 21:13:42 -0400

    shuffle data. "fresh" query param

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 const express = require('express');
 const shuffle = require('../lib/shuffle')
+const fetchCombinedData = require('../lib/fetch-combined-data')
 const router = express.Router();
 const env = process.env.NODE_ENV
 
-const getData = () => {
+const getData = (shouldBeFresh) => {
+  if (shouldBeFresh) {
+    return fetchCombinedData()
+  }
+
   return new Promise((resolve, reject) => {
     fs.readFile('./fetched-data/fetched-data.json', 'utf8', (err, data) => {
       err ? reject(err) : resolve(JSON.parse(data));
@@ -17,13 +22,15 @@ const mixData = (postAnalyticsData, messageData) => {
   // This mixes our set of data into
   // a nice little salad of happiness
   //
-  // It merges the two arrays by alternating through
+  // First it shuffles then the individual arrays' items.
+  // Then it merges the two arrays by alternating through
   // the items, merging them into one list.
   //
   // Right now it only supports mixing two lists.
+  // It's also pretty poorly written.
 
-  const arrA = [...postAnalyticsData]
-  const arrB = [...messageData]
+  const arrA = shuffle([...postAnalyticsData])
+  const arrB = shuffle([...messageData])
   const arrArr = [arrA, arrB]
   const limit = arrArr.reduce((a, b) => a + b.length, 0)
   const mixedData = []
@@ -41,13 +48,12 @@ const mixData = (postAnalyticsData, messageData) => {
     }
   }
 
-  // console.log(mixedData)
-  // return postAnalyticsData
   return mixedData
 }
 
 router.get('/', async (req, res, next) => {
-  const { data, fetchedAt } = await getData()
+  const fresh = req.query.fresh
+  const { data, fetchedAt } = await getData(fresh)
   const { postAnalyticsData, messageData } = data
 
   const typedPostAnalyticsData = postAnalyticsData.map(i => { return {
@@ -60,11 +66,11 @@ router.get('/', async (req, res, next) => {
   }})
 
   const mixedData = mixData(typedPostAnalyticsData, typedMessageData)
-  const firstFiveShuffledItems = mixedData.slice(0, 5)
+  const firstFewMixedItems = mixedData.slice(0, 10)
 
   const title = `High Fives`
 
-  res.render('index', { title, items: firstFiveShuffledItems, fetchedAt})
+  res.render('index', { title, items: firstFewMixedItems, fetchedAt})
 });
 
 module.exports = router;
